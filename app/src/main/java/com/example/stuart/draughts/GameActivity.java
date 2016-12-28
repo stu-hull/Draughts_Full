cccc7a29b642762f76932cce7b9868ead877300b
stu-hull <10s.hull@dsh-pms.com>
2016-12-28 15:24:30 +0000

    Changed dimensions to be relative to screen size, in preparation for coding counter position

--- app/src/main/java/com/example/stuart/draughts/GameActivity.java
+++ app/src/main/java/com/example/stuart/draughts/GameActivity.java
@@ -1,10 +1,15 @@
 package com.example.stuart.draughts;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -16,6 +21,10 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+
+        System.out.println("0");
+
         RelativeLayout layout = new RelativeLayout(this);
         layout.setBackgroundColor(getResources().getColor(R.color.background));
 
@@ -23,10 +32,12 @@ public class GameActivity extends AppCompatActivity {
         gameBoard.setId(R.id.gameBoard);
         gameBoard.setImageResource(R.drawable.boardv2);
         RelativeLayout.LayoutParams gameBoardParams = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
+                (int) (metrics.widthPixels - 50*metrics.density),
+                (int) (metrics.widthPixels - 50*metrics.density));
         gameBoardParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
         gameBoardParams.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        System.out.println(metrics.density);
 
         TextView player1Label = new TextView(this);
         player1Label.setId(R.id.player1Label);
@@ -38,6 +49,8 @@ public class GameActivity extends AppCompatActivity {
         player1LabelParams.addRule(RelativeLayout.BELOW, gameBoard.getId());
         player1LabelParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
 
+        System.out.println("2");
+
         TextView player2Label = new TextView(this);
         player2Label.setId(R.id.player2Label);
         player2Label.setText(R.string.player2);
@@ -48,11 +61,27 @@ public class GameActivity extends AppCompatActivity {
         player2LabelParams.addRule(RelativeLayout.ABOVE, gameBoard.getId());
         player2LabelParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
 
+        System.out.println("3");
+
+        ImageView counter0 = new ImageView(this);
+        counter0.setId(R.id.counter0);
+        counter0.setImageResource(R.drawable.redman);
+
+        RelativeLayout.LayoutParams counter0Params = new RelativeLayout.LayoutParams(
+                (int) ((metrics.widthPixels - 50*metrics.density)/8),
+                (int) ((metrics.widthPixels - 50*metrics.density)/8));
+        counter0Params.leftMargin = 100;
+        counter0Params.topMargin = 150;
+
+        System.out.println("4");
 
         layout.addView(gameBoard, gameBoardParams);
         layout.addView(player1Label, player1LabelParams);
         layout.addView(player2Label, player2LabelParams);
+        layout.addView(counter0, counter0Params);
         setContentView(layout);
+
         System.out.println("DONE");
     }
+
 }
